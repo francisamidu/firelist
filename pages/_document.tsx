c2bbd4f0d91ea0d726f828f332bb5c61508f91ed
francisamidu <francisamidu124@gmail.com>
2023-01-03 23:16:58 +0200

    Updated imports

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,6 +1,6 @@
 import { Html, Head, Main, NextScript } from "next/document";
 
-export default function Document() {
+const Document = () => {
   return (
     <Html>
       <Head>
@@ -21,4 +21,5 @@ export default function Document() {
       </body>
     </Html>
   );
-}
+};
+export default Document;
