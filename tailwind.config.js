b420e2b5e975aa87ea56eba8ee400fc9fc330be6
francisamidu <francisamidu124@gmail.com>
2023-01-03 23:26:25 +0200

    Fixed tailwind style issue

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,10 @@
 const withMT = require("@material-tailwind/react/utils/withMT");
 
 module.exports = withMT({
-  content: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+  ],
   theme: {
     extend: {},
   },
