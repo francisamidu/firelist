a3c013c45f1183944ce77f2fffdb5221e09639f1
francisamidu <francisamidu124@gmail.com>
2023-01-17 11:36:28 +0200

    Added ideas context provider

---
+++ contexts/index.ts
@@ -0,0 +1,4 @@
+import { IdeasProvider, useIdeas } from "./IdeasProvider";
+import { TodosProvider, useTodos } from "./TodosProvider";
+
+export { IdeasProvider, TodosProvider, useIdeas, useTodos };
