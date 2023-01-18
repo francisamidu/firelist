0a22af9bdb63f4bc5167198ed457967ef46f7495
francisamidu <francisamidu124@gmail.com>
2023-01-18 06:27:42 +0200

    feature:Working on idea page

--- components/Idea.tsx
+++ components/Idea.tsx
@@ -1,8 +1,19 @@
 import React from "react";
 import { IdeaProps } from "../types";
 
-const Idea = ({ getIdea, removeIdea, setIdeas, idea, ideas }: IdeaProps) => {
-  return <div>Idea</div>;
+const Idea = ({
+  getIdea,
+  removeIdea,
+  setIdeas,
+  idea: { title, description, tags },
+  ideas,
+}: IdeaProps) => {
+  return (
+    <div className="p-4 rounded-md bg-white">
+      <h1 className="text-3xl font-bold">{title}</h1>
+      <p className="text-gray-600 my-3">{description}</p>
+    </div>
+  );
 };
 
 export default Idea;
